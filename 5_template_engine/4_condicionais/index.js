0356530e6c00f4a3faea1ae2cfb0d569c8b16641
João VIctor Silva <joaovictordes02@gmail.com>
2024-08-11 02:26:39 -0300

    Condicionais

---
+++ 5_template_engine/4_condicionais/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+const usuario = {
+  name: "João Victor",
+  age: "21",
+};
+
+const auth = true;
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+app.get("/", (req, res) => {
+  res.render("home", { usuario: usuario, auth });
+});
+
+app.listen(3000, () => {
+  console.log("App funcionando!");
+});
