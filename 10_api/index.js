90cd6b6e7564def013b2419a765ccc6857ba6197
João Victor Silva <joaovictordes02@gmail.com>
2025-01-11 19:40:18 -0300

    POST api

--- 10_api/index.js
+++ 10_api/index.js
@@ -11,6 +11,13 @@ app.use(
 
 //endpoints
 
+app.post("/createproduct", (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  res.json({ message: `${name} ${price}` });
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Primeira rota" });
 });
